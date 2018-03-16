7a40b46a166a4887a95bc1b11807187d3ed2986b
c.perezc.2017 <claraketa@gmail.com>
2018-03-16 16:32:45 +0100

    initial upload of newfile.py

--- newfile.py
+++ newfile.py
@@ -1,8 +1,10 @@
 a =1
 b = 0
-
+c= 2
 print("Hello from pycharm 0")
 print("Hello from pycharm 1")
 print("Hello from pycharm 2")
 print("Hello from pycharm 3")
 print("Hello from pycharm 4")
+print("Hello from pycharm 6")
+print("Hello from pycharm 7")
