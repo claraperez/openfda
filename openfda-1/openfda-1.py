92a7eb15844a19c90b95466aa484ada2b1704f32
c.perezc.2017 <claraketa@gmail.com>
2018-04-08 17:33:02 +0200

    open fda-1

--- openfda-1/openfda-1.py
+++ openfda-1/openfda-1.py
@@ -4,6 +4,21 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
+conn.request("GET","/drug/label.json", None, headers)
+
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+drugs_raw = r1.read().decode("utf-8")
+conn.close()
+
+drugs = json.loads(drugs_raw)
+
+for result in drugs['results']:
+    print('The drugs id is', result['id'])
+    print('The drugs purpose is', result['purpose'])
+    print('The drugs manufacturer name is', result['openfda']['manufacturer_name'])
+
+conn = http.client.HTTPSConnection("api.fda.gov")
 conn.request("GET","/drug/label.json?limit=10", None, headers)
 
 r1 = conn.getresponse()
@@ -12,6 +27,7 @@ drugs_raw = r1.read().decode("utf-8")
 conn.close()
 
 drugs = json.loads(drugs_raw)
+
 for result in drugs['results']:
     print('The drugs id is', result['id'])
 
