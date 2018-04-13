0609b0ca3a1afd3d42403e8750206b3c360e0204
claraperez <claraketa@gmail.com>
2018-04-13 16:45:04 +0200

    openfda

--- openfda-3/openfda-3.py
+++ openfda-3/openfda-3.py
@@ -1,21 +1,50 @@
 import http.client
 import json
 
-headers = {'User-Agent': 'http-client'}
 
-conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET",'/drug/label.json?limit=10', None, headers)
+# HTTPRequestHandler class
+class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    # GET
+    def do_GET(self):
+        headers = {'User-Agent': 'http-client'}
 
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-drugs_raw = r1.read().decode("utf-8")
-conn.close()
+        conn = http.client.HTTPSConnection("api.fda.gov")
+        conn.request("GET",'/drug/label.json?limit=10', None, headers)
 
-drugs = json.loads(drugs_raw)
+        r1 = conn.getresponse()
+        print(r1.status, r1.reason)
+        drugs_raw = r1.read().decode("utf-8")
+        conn.close()
 
-i=0
-for drug_id in drugs['results']:
-    drug_id =  "<'ol'>" + drug[i]['id'] + "</ol>" + \
-    i+=1
+        drugs = json.loads(drugs_raw)
 
+        i=0
+        for drug_id in drugs['results']:
+            drug_id =  "<ol>" + drug[i]['id'] + "</ol>" + \
+            i=+1
+
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = drug_id
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+# Handler = http.server.SimpleHTTPRequestHandler
+Handler = testHTTPRequestHandler
+
+httpd = socketserver.TCPServer((IP, PORT), Handler)
+print("serving at port", PORT)
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+httpd.server_close()
 
